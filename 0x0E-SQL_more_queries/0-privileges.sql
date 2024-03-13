4222234eeb11576694668729d039aaff488d0985
SimonUwezo <simonnmwangi@gmail.com>
2024-03-13 13:22:42 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0dSHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
